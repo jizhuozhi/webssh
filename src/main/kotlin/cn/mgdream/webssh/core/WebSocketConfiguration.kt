497358319b1c1f3c6447ded6802f6c1a3235882e
jizhuozhi <jizhuozhi@jd.com>
2020-10-29 21:19:56 +0800

    [Draft] Enable WebSocket

--- src/main/kotlin/cn/mgdream/webssh/core/WebSocketConfiguration.kt
+++ src/main/kotlin/cn/mgdream/webssh/core/WebSocketConfiguration.kt
@@ -1,10 +1,12 @@
 package cn.mgdream.webssh.core
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
+@EnableWebSocket
 class WebSocketConfiguration(val webSshWebSocketHandler: WebSshWebSocketHandler) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(webSshWebSocketHandler, "/ws/webssh")
