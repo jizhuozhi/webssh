59ae1d0cd9abf73a24a4bf74d2778c59299088d1
jizhuozhi <jizhuozhi@jd.com>
2020-10-29 21:37:02 +0800

    [Draft] Terminal sends to and receives from server

--- src/main/resources/webssh/src/terminal.js
+++ src/main/resources/webssh/src/terminal.js
@@ -4,4 +4,11 @@ import 'xterm/css/xterm.css'
 window.onload = function (event) {
     const terminal = new Terminal()
     terminal.open(document.querySelector("#terminal"))
+    const socket = new WebSocket(location.origin.replace("http", "ws") + "/ws/webssh")
+    socket.onmessage = function (event) {
+        terminal.write(event.data)
+    }
+    terminal.onData(function (data) {
+        socket.send(data)
+    })
 }
