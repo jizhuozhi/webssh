299c90e34fe8e4a5eb98348a8eee73cdc161c5af
jizhuozhi <jizhuozhi@jd.com>
2020-11-05 11:30:00 +0800

    [Draft] Use FitAddon and stty to auto resize

--- src/main/resources/webssh/src/terminal.js
+++ src/main/resources/webssh/src/terminal.js
@@ -1,13 +1,24 @@
 import {Terminal} from "xterm";
+import {FitAddon} from "xterm-addon-fit";
 import 'xterm/css/xterm.css'
 
 window.onload = function (event) {
     const terminal = new Terminal()
+
+    const fitAddon = new FitAddon()
+    terminal.loadAddon(fitAddon)
+    window.onresize = function (event) {
+        fitAddon.fit()
+    }
+
     terminal.open(document.querySelector("#terminal"))
     const socket = new WebSocket(location.origin.replace("http", "ws") + "/ws/webssh")
     socket.onmessage = function (event) {
         terminal.write(event.data)
     }
+    terminal.onResize(function (event) {
+        socket.send(`stty cols ${event.cols} rows ${event.rows}\r`)
+    })
     terminal.onData(function (data) {
         socket.send(data)
     })
